737d706583a9eff1798df2a8eb37c448e605cb4c
Terriakidesu <trra53@gmail.com>
2024-05-04 14:19:47 +0800

    v3.0 start

---
+++ js/Preload.js
@@ -0,0 +1,31 @@
+
+class Preloader { };
+
+Preloader.preloadedAssets = {
+    "Images": {},
+    "Audios": {}
+};
+
+Preloader.assetList = {
+    images: [
+        "assets/Images/Background/dorito_bg_2.png",
+        "assets/Images/Background/dorito_bg_1.png",
+        "assets/Images/Nijika/Nijika_1.png",
+        "assets/Images/Nijika/Nijika_2_g.png",
+        "assets/Images/Nijika/Nijika_1_r.png",
+        "assets/Images/Nijika/Nijika_2_gray.png",
+        "assets/Images/Nijika/Nijika_2.png",
+        "assets/Images/Nijika/Nijika_1_g.png",
+        "assets/Images/Nijika/Nijika_1_b.png",
+        "assets/Images/Nijika/Nijika_1_gray.png",
+        "assets/Images/Nijika/Nijika_2_b.png",
+        "assets/Images/Nijika/Nijika_2_r.png",
+        "assets/Images/Mouse/mouse_black_1.png",
+        "assets/Images/Mouse/mouse_black_2.png",
+        "assets/Images/Dorito.png"
+    ],
+    audios: [
+        "assets/Audio/pop-39222.wav"
+    ]
+};
+
